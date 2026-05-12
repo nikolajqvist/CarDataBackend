c9e0653d57bd45600ae369b935cc781cf3dbebc2
nikolajqvist <nqvistmoller@hotmail.com>
2026-05-12 10:57:02 +0200

    jeg hader bytes

--- cardataapi/Controllers/CarDataSqliteController.cs
+++ cardataapi/Controllers/CarDataSqliteController.cs
@@ -25,19 +25,25 @@ namespace cardataapi.Controllers{
        }
        [HttpPost]
        [Route("logbikedata")]
-       public async Task<IActionResult> PostBD([FromBody] byte[] incomingText){
+       public async Task<IActionResult> PostBD([FromBody] byte[] incoming){
            // if(file.Length == 0 || file == null) return BadRequest();
            // IncomingFile incomingFile = new();
            // incomingFile.newTestFile = file;
            // await fileHandlerService.HandleBikeData(incomingFile);
            // if(string.IsNullOrEmpty(incomingText)) return BadRequest();
            // await stringHandlerService.AddBikeData(incomingText);
-           if(incomingText.Length == 0 || incomingText == null) return BadRequest();
-           await stringHandlerService.AddByteArray(incomingText);
+           if(incoming.Length == 0 || incoming == null) return BadRequest();
+           using var ms = new MemoryStream();
+           await Request.Body.CopyToAsync(ms);
+
+           byte[] incomingBytes = ms.ToArray();
+
+           if(incomingBytes.Length == 0 || incomingBytes == null) return BadRequest();
+           await stringHandlerService.AddByteArray(incomingBytes);
            return Ok("Very nice yub");
        }
        [HttpPost]
-       [Route("logsqlitescenario")]
+       [Route("logscenario")]
        public async Task<IActionResult> PostScenarios([FromForm] IFormFile file){
            if(file.Length == 0 || file == null) return BadRequest();
            IncomingFile incomingFile = new();
@@ -46,7 +52,7 @@ namespace cardataapi.Controllers{
            return Ok("Very nice yub");
        }
        [HttpPost]
-       [Route("logsqlitehtf")]
+       [Route("loghtf")]
        public async Task<IActionResult> PostPD([FromForm] IFormFile file){
            if(file.Length == 0 || file == null) return BadRequest();
            IncomingFile incomingFile = new();
@@ -55,7 +61,7 @@ namespace cardataapi.Controllers{
            return Ok("Very nice yub");
        }
        [HttpPost]
-       [Route("logsqlitearduino")]
+       [Route("logarduino")]
        public async Task<IActionResult> PostArduino([FromForm] IFormFile file){
            if(file.Length == 0 || file == null) return BadRequest();
            IncomingFile incomingFile = new();
