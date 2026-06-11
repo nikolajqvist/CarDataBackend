d6fb83ec400d182f2d97cde37c5a1ce026c2de97
nikolajqvist <nqvistmoller@hotmail.com>
2026-06-11 09:38:01 +0200

    Backup api done

---
+++ cardataapi/Controllers/CarDataMssqlController.cs
@@ -0,0 +1,102 @@
+using CARDataLib;
+using System.IO;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Data.Sqlite;
+using System.Reflection.Metadata;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace cardataapi.Controllers
+{
+    [Route("api/cardata/backup")]
+    [ApiController]
+    public class CarDataMssqlController : ControllerBase
+    {
+        private CarDataMssqlRepository carDataRepository;
+        private CarDataChunkRepository chunkRepository;
+        private MssqlByteHandlerService mssqlByteHandlerService;
+        public CarDataMssqlController(CarDataMssqlRepository carDataRepository, CarDataChunkRepository chunkRepository, MssqlByteHandlerService mssqlByteHandlerService)
+        {
+            this.carDataRepository = carDataRepository;
+            this.chunkRepository = chunkRepository;
+            this.mssqlByteHandlerService = mssqlByteHandlerService;
+        }
+        // GET: api/<PostController>
+        [HttpGet]
+        [Route("id/{userId}")]
+        public IActionResult GetUser(int userId)
+        {
+            User user = carDataRepository.GetUser(userId);
+            if(user != null)
+                return Ok(user);
+            else
+                return BadRequest();
+        }
+       [HttpPost]
+       [Route("logbikedata")]
+       public async Task<IActionResult> BackupBD(){
+           using var ms = new MemoryStream();
+           await Request.Body.CopyToAsync(ms);
+
+           byte[] incomingBytes = ms.ToArray();
+
+           if(incomingBytes.Length == 0 || incomingBytes == null) return BadRequest();
+           await mssqlByteHandlerService.ByteBikeData(incomingBytes);
+           return Ok("Bike data indsat. /Backup");
+       }
+       [HttpPost]
+       [Route("logscenario")]
+       public async Task<IActionResult> BackupScenario(){
+           using var ms = new MemoryStream();
+           await Request.Body.CopyToAsync(ms);
+
+           byte[] incomingBytes = ms.ToArray();
+
+           if(incomingBytes.Length == 0 || incomingBytes == null) return BadRequest();
+           await mssqlByteHandlerService.ByteScenarios(incomingBytes);
+           return Ok("Scenario data indsat. /Backup");
+       }
+       [HttpPost]
+       [Route("loghtf")]
+       public async Task<IActionResult> BackupHtf(){
+           using var ms = new MemoryStream();
+           await Request.Body.CopyToAsync(ms);
+
+           byte[] incomingBytes = ms.ToArray();
+
+           if(incomingBytes.Length == 0 || incomingBytes == null) return BadRequest();
+           await mssqlByteHandlerService.ByteHeadTransform(incomingBytes);
+           return Ok("Headtransform data indsat. /Backup");
+       }
+       [HttpPost]
+       [Route("logarduino")]
+       public async Task<IActionResult> BackupArduino(){
+           using var ms = new MemoryStream();
+           await Request.Body.CopyToAsync(ms);
+
+           byte[] incomingBytes = ms.ToArray();
+
+           if(incomingBytes.Length == 0 || incomingBytes == null) return BadRequest();
+           await mssqlByteHandlerService.ByteArduino(incomingBytes);
+           return Ok("Arduino data indsat. /Backup");
+       }
+       [HttpPost]
+       [Route("logpulse")]
+       public async Task<IActionResult> BackupPulseData(){
+           using var ms = new MemoryStream();
+           await Request.Body.CopyToAsync(ms);
+
+           byte[] incomingBytes = ms.ToArray();
+
+           if(incomingBytes.Length == 0 || incomingBytes == null) return BadRequest();
+
+           await mssqlByteHandlerService.BytePulseData(incomingBytes);
+
+           return Ok("Pulse data indsat. /Backup");
+       }
+
+    }
+}
+
---
+++ cardataapi/InputHandling/MssqlByteHandlerService.cs
@@ -0,0 +1,135 @@
+using CARDataLib;
+using System.Text;
+namespace cardataapi;
+
+public class MssqlByteHandlerService {
+    private CarDataSqliteChunkRepository carDataChunkRepository;
+    public MssqlByteHandlerService(CarDataSqliteChunkRepository carDataChunkRepository){
+        this.carDataChunkRepository = carDataChunkRepository;
+    }
+    public async Task ByteScenarios(byte[] unityarray){
+        ValidateByteArray(unityarray);
+
+        string fromByteToString = Encoding.UTF8.GetString(unityarray);
+        string[] splitString = fromByteToString.Split("|");
+        string firstLine = splitString[0];
+        int.TryParse(firstLine, out int id);
+        var chunks = splitString.Skip(1).Chunk(3);
+        List<Scenario> scenarios = new List<Scenario>();
+        foreach(var chunk in chunks){
+        if(chunk.Length <= 0) continue;
+            if(chunk.Length < 3) throw new ArgumentOutOfRangeException("Chunk skal minimum være 3 lang");
+            string scename = chunk[0].ToString();
+            double cyclToDis = double.Parse(chunk[1]);
+            DateTime start = DateTime.Parse(chunk[2]);
+            DateTime end = DateTime.Parse(chunk[3]);
+            Scenario sce = new Scenario();
+            sce.ScenarioName = scename;
+            sce.CycleToCarDistance = cyclToDis;
+            sce.ScenarioStart = start;
+            sce.ScenarioEnd = end;
+            scenarios.Add(sce);
+        }
+        await carDataChunkRepository.AddScenarios(scenarios, id);
+    }
+    public async Task ByteBikeData(byte[] unityarray){
+        ValidateByteArray(unityarray);
+        string fromByteToString = Encoding.UTF8.GetString(unityarray);
+        string[] splitString = fromByteToString.Split("|");
+        string firstLine = splitString[0];
+        int.TryParse(firstLine, out int id);
+        var chunks = splitString.Skip(1).Chunk(3);
+        List<BikeData> bikeDatas = new List<BikeData>();
+        foreach(var chunk in chunks){
+        if(chunk.Length <= 0) continue;
+            if(chunk.Length < 3) throw new ArgumentOutOfRangeException("Chunk skal minimum være 3 lang");
+            double yRot = double.Parse(chunk[0]);
+            double curb = double.Parse(chunk[1]);
+            double speed = double.Parse(chunk[2]);
+            BikeData b = new BikeData();
+            b.HandleRotationY = yRot;
+            b.DistanceCurbSide = curb;
+            b.Speed = speed;
+            bikeDatas.Add(b);
+        }
+        await carDataChunkRepository.AddBikeData(bikeDatas, id);
+    }
+    public async Task ByteHeadTransform(byte[] unityarray){
+        ValidateByteArray(unityarray);
+        string fromByteToString = Encoding.UTF8.GetString(unityarray);
+        string[] splitString = fromByteToString.Split("|");
+        string firstLine = splitString[0];
+        int.TryParse(firstLine, out int id);
+        var chunks = splitString.Skip(1).Chunk(7);
+        List<HeadTransform> hTfs = new List<HeadTransform>();
+        foreach(var chunk in chunks){
+            if(chunk.Length <= 0) continue;
+            if(chunk.Length < 7) throw new ArgumentOutOfRangeException("Chunk skal være minimum 7");
+            double rw = double.Parse(chunk[0]);
+            double rz = double.Parse(chunk[1]);
+            double rx = double.Parse(chunk[2]);
+            double ry = double.Parse(chunk[3]);
+            double pz = double.Parse(chunk[4]);
+            double px = double.Parse(chunk[5]);
+            double py = double.Parse(chunk[6]);
+            HeadTransform htf = new HeadTransform();
+            htf.RotW = rw;
+            htf.RotZ = rz;
+            htf.RotX = rx;
+            htf.RotY = ry;
+            htf.PosZ = pz;
+            htf.PosX = px;
+            htf.PosY = py;
+            hTfs.Add(htf);
+        }
+        await carDataChunkRepository.AddHeadTransform(hTfs, id);
+    }
+    public async Task ByteArduino(byte[] unityarray){
+        ValidateByteArray(unityarray);
+        string fromByteToString = Encoding.UTF8.GetString(unityarray);
+        string[] splitString = fromByteToString.Split("|");
+        string firstLine = splitString[0];
+        int.TryParse(firstLine, out int id);
+        var chunks = splitString.Skip(1).Chunk(4);
+        List<LeftBrake> leftBrakes = new List<LeftBrake>();
+        List<RightBrake> rightBrakes = new List<RightBrake>();
+        foreach(var chunk in chunks){
+            bool left = bool.Parse(chunk[0]);
+            bool right = bool.Parse(chunk[1]);
+            DateTime dT = DateTime.Parse(chunk[2]); 
+            RightBrake rb = new RightBrake();
+            LeftBrake lb = new LeftBrake();
+            lb.LeftBraking = left;
+            lb.BrakeTime = dT;
+            rb.RightBraking = right;
+            rb.BrakeTime = dT;
+            leftBrakes.Add(lb);
+            rightBrakes.Add(rb);
+        }
+        await carDataChunkRepository.AddLeftBrake(leftBrakes, id);
+        await carDataChunkRepository.AddRigthBrake(rightBrakes, id);
+    }
+    public async Task BytePulseData(byte[] unityarray){
+        ValidateByteArray(unityarray);
+        string fromByteToString = Encoding.UTF8.GetString(unityarray);
+        string[] splitString = fromByteToString.Split("|");
+        string firstLine = splitString[0];
+        int.TryParse(firstLine, out int id);
+        var chunks = splitString.Skip(1).Chunk(2);
+        List<PulseData> pulseDatas = new List<PulseData>();
+        foreach(var chunk in chunks){
+            int pulse = int.Parse(chunk[0]);
+            DateTime dateTime = DateTime.Parse(chunk[1]);
+            PulseData p = new PulseData();
+            p.Pulse = pulse;
+            p.PulseTime = dateTime;
+            pulseDatas.Add(p);
+        }
+        await carDataChunkRepository.AddPulseData(pulseDatas, id);
+    }
+    private void ValidateByteArray(byte[] tovalidate){ 
+        if(tovalidate.Length == 0 || tovalidate == null){
+            throw new ArgumentNullException("Intet i array"); 
+        } 
+    }
+}
